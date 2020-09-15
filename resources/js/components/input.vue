110630d7acfde728f7ff267c9e8e1a803451bef8
york <jason945119@gmail.com>
2020-09-15 17:37:32 +0800

    測試

---
+++ resources/js/components/input.vue
@@ -0,0 +1,8 @@
+<template>
+    <script type="x-template" id="form-input">
+        <div>
+            <label>Text</label>
+            <input type="text"/>
+        </div>
+    </script>
+</template>
--- resources/views/york.blade.php
+++ resources/views/york.blade.php
@@ -12,9 +12,14 @@
 
 </head>
 <body>
-<div id="york-div">
-york
+<!-- 準備給 Vue 的掛載點 -->
+<div id="app">
+    <!-- 使用我們建立的元件，一個有傳入名字，一個沒有 -->
+    <Hello name="Tony"></Hello>
 </div>
+
+<!-- 載入打包後的 js 檔 -->
+<script src="/js/hello.js"></script>
 
 <!-- Page Js-->
 @yield('pageJs')
